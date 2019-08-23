334f38ac17adbc9f46d1f08de0672c592bb260a6
Ryan Karpeles <ryan@teamodea.com>
2019-08-22 21:49:04 -0500

    Safari video fix

--- wp-content/themes/depardo/footer.php
+++ wp-content/themes/depardo/footer.php
@@ -18,16 +18,24 @@
 	<div class="container">
 		<div class="grid">
 			<div class="col col--1-3">
-				<h3 class="footer__subhead">Subhead</h3>
-				<p class="footer__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex e</p>
+				<img class="footer__logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logos/logo-red.svg" alt="">
+				<p class="footer__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex.</p>
+				<p><a class="footer__link" href="#">Read More ></a></p>
 			</div>
 			<div class="col col--1-3">
-				<h3 class="footer__subhead">Subhead</h3>
-				<p class="footer__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex e</p>
+				<h3 class="footer__subhead">/ Recent Posts</h3>
+				<p class="footer__text">
+				  Month 01, 2019<br>
+				  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.
+			  </p>
+			  <p class="footer__text">
+				  Month 01, 2019<br>
+				  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.
+			  </p>
 			</div>
 			<div class="col col--1-3">
-				<h3 class="footer__subhead">Subhead</h3>
-				<p class="footer__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex e</p>
+				<h3 class="footer__subhead">/ Subhead</h3>
+				<p class="footer__text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex consectetuer adipiscing elit, sed diam nonummy nibh euismod.</p>
 			</div>
 		</div>
 	</div>
--- wp-content/themes/depardo/page-homepage.php
+++ wp-content/themes/depardo/page-homepage.php
@@ -13,96 +13,114 @@ get_header();
     <div id="primary" class="content-area">
       <main id="main" class="site-main">
 
-        <section class="hero hero--video">
-          <video id="videoBG" poster="poster.JPG" autoplay muted loop>
-            <source src="<?php echo get_stylesheet_directory_uri(); ?>/video/bg-video.mp4" type="video/mp4">
-          </video>
-          <div class="container">
-            <div class="hero__text">
-              <h1 class="hero__title">Fuel success</h1>
-              <p class="hero__copy">Get data-driven strategies to fire up short-term wins and long-term performance</p>          
-            </div>
-          </div>        
-        </section>
+        <?php      
+          $hero = get_field('hero'); 
+          if( $hero ): ?>
 
-        <section class="triple-vert-padding">
-          <div class="container">
-            <div class="grid text-centered">
-              <div class="col col--1-3">
-                <div class="icon-block">
-                  <img class="icon-block__image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-man-graph.svg" alt="alt text">
-                  <h2 class="icon-block__heading">Is your business performing the way you had imagined?</h2>
+            <section class="hero hero--video">
+              <video id="videoBG" poster="poster.JPG" autoplay muted loop>
+                <source src="<?php echo $hero['background_video']['url']; ?>" type="video/mp4">
+              </video>
+              <div class="container">
+                <div class="hero__text">
+                  <h1 class="hero__title"><?php echo $hero['heading']; ?></h1>
+                  <p class="hero__copy"><?php echo $hero['subheading']; ?></p>          
+                </div>
+              </div>        
+            </section>
+
+        <?php endif; ?> 
+
+        <?php      
+          $columns = get_field('columns');  
+          if( $columns ): ?>       
+
+            <section class="triple-vert-padding">
+              <div class="container">
+                <div class="grid text-centered">
+                  <div class="col col--1-3">
+                    <div class="icon-block">
+                      <img class="icon-block__image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-man-graph.svg" alt="alt text">
+                      <h2 class="icon-block__heading"><?php echo $columns['column_1_text']; ?></h2>
+                    </div>
+                  </div>
+                  <div class="col col--1-3">
+                    <div class="icon-block">
+                      <img class="icon-block__image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-man-clock.svg" alt="alt text">
+                      <h2 class="icon-block__heading"><?php echo $columns['column_2_text']; ?></h2>
+                    </div>
+                  </div>
+                  <div class="col col--1-3">
+                    <div class="icon-block">
+                      <img class="icon-block__image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-head-question.svg" alt="alt text">
+                      <h2 class="icon-block__heading"><?php echo $columns['column_3_text']; ?></h2>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="col col--1-3">
-                <div class="icon-block">
-                  <img class="icon-block__image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-man-clock.svg" alt="alt text">
-                  <h2 class="icon-block__heading">Are you well-positioned for the future?</h2>
+            </section>
+
+        <?php endif; ?> 
+
+        <?php      
+          $interstitial = get_field('interstitial');  
+          if( $interstitial ): ?>
+
+            <section class="featured-block has-overlay-dark" style="background-image: url('<?php echo $interstitial['background_image']['url']; ?>');">
+              <div class="container">
+                <div class="featured-block__content">
+                  <div class="featured-block__image">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-bulb-arrow.svg" alt="alt text">
+                  </div>
+                  <div class="featured-block__heading">
+                    <h4><?php echo $interstitial['heading']; ?></h4>
+                  </div>
+                  <div class="featured-block__text">
+                    <?php echo $interstitial['copy']; ?>
+                  </div>
                 </div>
               </div>
-              <div class="col col--1-3">
-                <div class="icon-block">
-                  <img class="icon-block__image" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/icon-head-question.svg" alt="alt text">
-                  <h2 class="icon-block__heading">Are your people reaching their maximum potential?</h2>
+            </section>
+
+        <?php endif; ?>
+
+        <?php      
+          $imagetext = get_field('image_text_panel'); 
+          if( $imagetext ): ?>
+
+            <section class="double-vert-padding">
+              <div class="container">
+                <div class="grid">
+                  <div class="col col--1-2">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/stock/bird-looks-right.png" alt="alt text">
+                  </div>
+                  <div class="col col--1-2">
+                    <h4 class="color--primary"><?php echo $imagetext['heading']; ?></h4>
+                    <?php echo $imagetext['copy']; ?>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </section>
+            </section>
 
-        <section class="featured-block" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/img/banners/banner-leaf.jpg');">
-          <div class="container">
-            <div class="featured-block__content">
-              <div class="featured-block__image">
-                <img src="" alt="">
-              </div>
-              <div class="featured-block__heading">
-                <h4>Drive Positive Change</h4>
-              </div>
-              <div class="featured-block__text">
-                <p>De Pardo Consulting understands your challenges. As specialists who assess performance industry-wide, we know how to drive positive change in your business.</p>
-                <p>We work with financial advisory firms, both directly and in partnership with financial institutions. We bring a unique perspective, honed from years of expertise and research, that ignites growth and fuels long-term success.</p>
-              </div>
-            </div>
-          </div>
-        </section>
+        <?php endif; ?>
 
-        <section>
-          <div class="container">
-            <div class="grid">
-              <div class="col col--1-2">
-                <p>Test</p>
-              </div>
-              <div class="col col--1-2">
-                <h4>Let's Get Started</h4>
-                <p>We start by listening to you, talking with your team, and striving
-to completely understand your unique set of circumstances.
-This allows us to quickly pinpoint any deficiencies and
-distinguish your strengths and opportunities. Our custom-built
-advice provides a set of recommendations for your specific
-needs and capabilities, coupled with implementation planning
-that propels short-term wins and achieves your long-term
-aspirations.
-Whether you’re a financial advisory firm or a financial
-institution, our work is only effective if it truly gives you the tools
-to create value and long-term success.
-It’s the old adage: We don’t look good unless you look good</p>
-              </div>
-            </div>
-          </div>
-        </section>
+        <?php      
+          $cta = get_field('cta_block'); 
+          if( $cta ): ?>
 
-        <section class="cta-block">
-          <div class="container">
-            <div class="cta-block__text">
-              <h4>Ignite Growth</h4>
-              <p>Put your firm on a path to outperform expectations</p>
-            </div>
-            <div class="cta-block__button">
-              <a href="#" class="btn">Contact Us</a>
-            </div>
-          </div>
-        </section>
+            <section class="cta-block">
+              <div class="container">
+                <div class="cta-block__text">
+                  <h4><?php echo $cta['heading']; ?></h4>
+                  <p><?php echo $cta['subheading']; ?></p>
+                </div>
+                <div class="cta-block__button">
+                  <a href="#" class="btn">Contact Us</a>
+                </div>
+              </div>
+            </section>
+
+        <?php endif; ?>
 
       </main><!-- #main -->
     </div><!-- #primary -->    
